59bd2462573c9749fafb0b1eda61c89e88943191
P3dro08885 <nathanpbacelar2009@gmail.com>
2026-08-07 17:45:43 -0300

    Add files via upload

---
+++ -TCC-/-TCC-/-TCC-/index.php
@@ -0,0 +1,27 @@
+<?php
+include __DIR__.'/includes/head.php';
+?>
+<section class="Tela">
+<div class="Titulo">
+<h1 class="Nome-Titulo">Zubbo</h1>
+</div>
+
+<div class="buttons">
+<button onclick="window.location.href='login.php'" style="margin-right:15px" >
+  <span class="shadow"></span>
+  <span class="edge2"></span>
+  <span class="front2"> Login
+  </span>
+</button>
+<button onclick="window.location.href='cadastrar.php'">
+  <span class="shadow"></span>
+  <span class="edge"></span>
+  <span class="front text"> Cadastrar
+  </span>
+</button>
+</div>
+</section>
+
+<?php
+include __DIR__.'/includes/footer.php';
+?>
