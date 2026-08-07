3b63f03e0b9e5134ee5876e9036ada8b6232bfe3
P3dro08885 <nathanpbacelar2009@gmail.com>
2026-08-07 17:53:15 -0300

    Add files via upload

---
+++ -TCC-/login.php
@@ -0,0 +1,18 @@
+<?php
+include __DIR__.'/includes/head.php';
+?>
+
+<div class="Titulo">
+<h1 class="Nome-Titulo">Login Aqui</h1>
+</div>
+
+<?php
+include __DIR__.'/includes/footer.php';
+?>
+
+<!--
+    <div class="field">
+        <label>LOGIN</label>
+        <input type="text" placeholder="digite seu ID" name="login_txt" require>
+    </div>
+      -->
