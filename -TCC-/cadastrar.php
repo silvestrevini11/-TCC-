aaf2c59f984e69e2c6b71d1f1c7e28942dda519c
YcaroJesus <ycaro.silva3@etec.sp.gov.br>
2026-08-10 13:51:36 -0300

    Add files via upload

--- -TCC-/cadastrar.php
+++ -TCC-/cadastrar.php
@@ -2,13 +2,49 @@
 include __DIR__.'/includes/head.php';
 ?>
 
-<div class="Titulo">
-<h1 class="Nome-Titulo">Criar Conta</h1>
+<section class="Tela">
+
+<div class="Titulo-cad">
+<h1 class="cad-nome" style="text-shadow: 2px 1px 3px white; ">Criar<strong style="color: black; text-shadow: 2px 1px 3px black; "></strong> <strong style="color: black; text-shadow: 2px 1px 3px black; ">Conta</strong>
+</div>
+<div class="cad-sub">
+    <h2 class="cad-sub-txt"><strong>Crie uma conta para que você possa explorar conosco.</strong></h2>
 </div>
 
+    <div class="field" style="margin-top:200px;">
+        <label>Email</label>
+        <input type="Email" placeholder="Email" name="Email" require>
+    </div>
+    <div class="field">
+        <label>Senha</label>
+        <input type="Password" placeholder="Senha" name="Password" require>
+    </div>
+    <div class="field">
+        <label>Senha Novamente</label>
+        <input type="Password" placeholder="Confirme a Senha" name="Password_again" require>
+    </div>
+
+    <div class="but" style="margin-top:75px;">
+        <button onclick="window.location.href='cad-user-confirmar.php'">
+            <span class="shadow"></span>
+            <span class="edge2"></span>
+            <span class="front2">Inscreva-se
+</span>
+        </button>
+    </div>
+    <div class="but">
+        <button onclick="window.location.href='login.php'">
+            <span class="shadow"></span>
+            <span class="edge"></span>
+            <span class="front text">Já tenho uma conta</span>
+        </button>
+    </div>
+    
 <?php
 include __DIR__.'/includes/footer.php';
 ?>
+
+</section>
 
 <!--
     <div class="field">
--- -TCC-/login.php
+++ -TCC-/login.php
@@ -2,13 +2,51 @@
 include __DIR__.'/includes/head.php';
 ?>
 
-<div class="Titulo">
-<h1 class="Nome-Titulo">Login Aqui</h1>
+<section class="Tela">
+
+<div class="Titulo-log">
+<h1 class="login-nome" style="text-shadow: 2px 1px 3px white; "><strong style="color: black; text-shadow: 2px 1px 3px black; ">L</strong>ogin <strong style="color: black; text-shadow: 2px 1px 3px black; ">A</strong>qui
 </div>
+<div class="log-sub">
+    <h2 class="log-sub-txt"><strong>Bem-vindo de volta, sentimos sua falta!</strong></h2>
+</div>
+
+
+
+    <div class="field" style="margin-top:200px;">
+        <label>Email</label>
+        <input type="Email" placeholder="Email" name="Email" require>
+    </div>
+    <div class="field">
+        <label>Senha</label>
+        <input type="Password" placeholder="Senha" name="Password" require>
+    </div>
+    
+
+    <div  class="log-sub">
+        <h2 style="color: white;">Esqueceu sua Senha?</h2>
+    </div  class="log-sub">
+
+    <div class="but">
+        <button onclick="window.location.href='log-user-confirmar.php'">
+            <span class="shadow"></span>
+            <span class="edge2"></span>
+            <span class="front2">Entrar</span>
+        </button>
+    </div>
+    <div class="but">
+        <button onclick="window.location.href='cadastrar.php'">
+            <span class="shadow"></span>
+            <span class="edge"></span>
+            <span class="front text">Criar nova conta</span>
+        </button>
+    </div>
 
 <?php
 include __DIR__.'/includes/footer.php';
 ?>
+
+</section>
 
 <!--
     <div class="field">
