80f8e099db1665a656b5f8a22a0067f333b8b582
YcaroJesus <ycaro.silva3@etec.sp.gov.br>
2026-08-20 15:08:01 -0300

    Login-Css Update

--- -TCC-/login.php
+++ -TCC-/login.php
@@ -4,11 +4,11 @@ include 'includes/head.php';
 
 <main class="login-container">
 
-    <img src="imagem/LogooZ.png" alt="Logo Zubbo" class="logo">
+    <img src="imagem/LogooZ.png" alt="Logo Zubbo" class="login-logo">
 
     <h1>Entrar</h1>
 
-    <p class="subtitle">Que bom te ver de novo!</p>
+    <p class="login-subtitle">Que bom te ver de novo!</p>
 
     <form action="" method="POST">
 
@@ -26,7 +26,7 @@ include 'includes/head.php';
             required
         >
 
-        <a href="#" class="forgot-password">
+        <a href="#" class="login-forgot-password">
             Esqueceu sua senha?
         </a>
 
@@ -36,24 +36,24 @@ include 'includes/head.php';
 
     </form>
 
-    <div class="divider">
+    <div class="login-divider">
         <span></span>
         <strong>ou</strong>
         <span></span>
     </div>
 
-    <button class="social-button facebook">
-        <span class="facebook-icon">f</span>
+    <button class="login-social-button facebook">
+        <span class="login-facebook-icon">f</span>
         <span>Continuar com Facebook</span>
     </button>
 
-    <button class="social-button google">
-        <span class="google-icon">G</span>
+    <button class="login-social-button google">
+        <span class="login-google-icon">G</span>
         <span>Continuar com Google</span>
     </button>
 
-    <button class="social-button apple">
-        <span class="apple-icon" aria-hidden="true">
+    <button class="login-social-button apple">
+        <span class="login-apple-icon" aria-hidden="true">
             <svg viewBox="0 0 24 24" role="img" aria-label="Apple">
                 <path d="M16.7 12.1c0-2.1 1.7-3.1 1.8-3.2-1-1.4-2.5-1.6-3-1.6-1.3-.1-2.5.8-3.1.8-.6 0-1.6-.8-2.7-.8-1.4 0-2.7.8-3.4 2.1-1.5 2.5-.4 6.3 1 8.3.7 1 1.5 2.1 2.6 2 .9 0 1.3-.6 2.5-.6s1.5.6 2.5.6c1.1 0 1.8-1 2.5-2 .8-1.1 1.1-2.2 1.1-2.3-.1 0-2.1-.8-2.1-3.3ZM14.7 6.1c.6-.8 1-1.8.9-2.9-.9 0-2 .6-2.6 1.4-.6.7-1.1 1.7-1 2.7 1 .1 2-.5 2.7-1.2Z" />
             </svg>
